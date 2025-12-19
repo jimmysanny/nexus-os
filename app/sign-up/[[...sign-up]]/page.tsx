c9977f3b4c3ba3a4c67a092aa3c19321fb3f9ea7
James Nexus <james@example.com>
2025-12-19 02:54:27 +0200

    Added sign-in and sign-up pages

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+﻿import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-black">
+      <SignUp />
+    </div>
+  );
+}
