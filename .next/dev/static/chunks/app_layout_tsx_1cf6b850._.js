09d915303b1469b96bc35cbd83c625018f5bdaf9
James Nexus <james@example.com>
2025-12-28 22:14:46 +0200

    feat: add email testing tool

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++ .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_456084cf._.js",
-  "static/chunks/node_modules_36d64b3e._.js",
+  "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_ce3a139a._.js",
+  "static/chunks/node_modules_6bf6d6ae._.js",
   "static/chunks/app_globals_71f961d1.css"
 ],
     source: "dynamic"
