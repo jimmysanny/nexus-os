e43c66f9198d3529014f57271303d1b757c58adf
James Nexus <james@example.com>
2025-12-29 14:03:57 +0200

    chore: remove unused edgestore files

--- lib/edgestore.ts
+++
@@ -1,8 +0,0 @@
-﻿'use client';
-
-import { type EdgeStoreRouter } from '@/app/api/edgestore/[...edgestore]/route';
-import { createEdgeStoreProvider } from '@edgestore/react';
-
-const { EdgeStoreProvider, useEdgeStore } = createEdgeStoreProvider<EdgeStoreRouter>();
-
-export { EdgeStoreProvider, useEdgeStore };
