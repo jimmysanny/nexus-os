c4bdef3700cde98a4fdf38db333febde8a6dc9e6
James Nexus <james@example.com>
2025-12-30 23:14:40 +0200

    fix: hard delete of conflicting middleware and config files

--- .next/dev/server/edge/chunks/[root-of-the-server]__f2b15f93._.js
+++ .next/dev/server/edge/chunks/[root-of-the-server]__f2b15f93._.js
@@ -25,9 +25,7 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f40$clerk$2f$n
 const __TURBOPACK__default__export__ = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f40$clerk$2f$nextjs$2f$dist$2f$esm$2f$server$2f$clerkMiddleware$2e$js__$5b$middleware$2d$edge$5d$__$28$ecmascript$29$__["clerkMiddleware"])();
 const config = {
     matcher: [
-        // Skip Next.js internals and all static files
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
         '/(api|trpc)(.*)'
     ]
 };
