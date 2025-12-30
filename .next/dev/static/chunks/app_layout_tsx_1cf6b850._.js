3eff20c481ae74d2d2119188a99be1b54b8f6345
James Nexus <james@example.com>
2025-12-30 23:32:42 +0200

    fix: nuclear git index reset to purge ghost middleware

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__36b083e1._.css",
-  "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_1a5fa2d2._.js",
-  "static/chunks/node_modules_36d64b3e._.js"
-],
-    source: "dynamic"
-});
