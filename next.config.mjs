0ab4ec207e0d8a6a7ca13b011b633781f8e6e53b
James Nexus <james@example.com>
2025-12-30 16:32:40 +0200

    fix: sanitize config to stop range error crash

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const nextConfig = {
       { protocol: "https", hostname: "utfs.io" },
       { protocol: "https", hostname: "img.clerk.com" }
     ]
-  },
-  typescript: {
-    ignoreBuildErrors: true,
   }
 };
 
